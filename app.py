7513d8d76ffc26b9beadd785f426ae3aa427b386
kdd <klaus-dieter@doell-online.de>
2024-01-28 20:30:27 +0100

    support swagger interface

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, request, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 import os
 # import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '/')
+
+SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'swagger.json'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
 
 wz08_codes = [
     {"WZ08": 2573, "Beschreibung": "Herstellung von Werkzeugen", "Abschnitt": "C"},
@@ -109,4 +130,5 @@ if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
+    app.register_blueprint(swaggerui_blueprint)
     app.run(port=port, host='0.0.0.0')
